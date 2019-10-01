05baafeb66a58041ccc2185d13b0ab8d3aa7c0fe
selin ovali <sovali@purdue.edu>
2019-10-01 17:26:30 -0400

    login form created

--- purduesale/src/Login.js
+++ purduesale/src/Login.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react'
-import Link from 'react-router-dom'
+//import Link from 'react-router-dom'
+
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            authenticated: false,
+        }
+        this.authenticate = this.authenticate.bind(this)
+    }
+
+    authenticate(e) {
+        e.preventDefault();
+        console.table([{
+            email:this.emailInput.value,
+            password: this.passwordInput.value,
+        }])
+
+        console.log("hey!");
+    }
+
     render() {
         return (
             <div>
-
-               {this.props.authenticated
-               ? <a> you're logged in!!! </a>
-               : <input className="pt-input" placeholder="Type your email..." type="email"/>
-            
+                <form onSubmit={e => this.authenticate(e) } ref={(form) => { this.loginForm = form }}>               {
+                   this.props.authenticated
+               ? (<p> you're logged in!!! </p>)
+               : ( <div> 
+                   <label className="pt-label">
+                    Email
+                    <br/>
+                    <input className="pt-input" name="email" placeholder="Type your email..." type="email" ref={(input) => {this.emailInput = input}}/>
+                    </label>
+                    <br/>
+                    <label className="pt-label">
+                    Password
+                    <br/>
+                    <input className="pt-input" name="password" placeholder="Type your password..." type="password" ref={(input) => {this.passwordInput = input}}/>
+                    </label>
+                    <br/>
+                    <input style={{width: "100%"}} type="submit" className="pt-button pt-intent-primary" value="Log In"></input>  
+               </div>
+               )
             }
-
+            </form>
             </div>
 
         )
