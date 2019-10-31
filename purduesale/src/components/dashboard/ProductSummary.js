b8b10f930b9ee79ad1c5aabb61f6b62fc1089bc4
Stefany Go <stefany_go@hotmail.com>
2019-10-31 17:15:40 -0400

    add uniform card height to display products

--- purduesale/src/components/dashboard/ProductSummary.js
+++ purduesale/src/components/dashboard/ProductSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import userlogo1 from './userlogo1.png'
+import './ProductSummary.css';
 
 const ProductSummary = ({product}) => {
     return(
@@ -7,15 +8,15 @@ const ProductSummary = ({product}) => {
         <div class="card">
             <div class="card-image">
                 <img className="Image" src={product.imageUrl || userlogo1} alt={product.ProductName}  />
-                
+                <div class="card-content">
+                <span class="title">{product.productName}</span>
+                <p>{product.description}</p>
             </div>
-      <div class="card-content">
-      <span class="card-title">{product.productName}</span>
-        <p>{product.description}</p>
-      </div>
-      <b>{product.price}</b>
-      <div class="card-action">
-        <a >Add to Cart</a>
+            <b>{product.price}</b>
+            <div class="card-action">
+            <a >Add to Cart</a>
+            </div>
+            
         </div>
 </div>
     )
